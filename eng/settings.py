706289639c793b62993629b3d2cf31c6f034268a
ajesh777mn <kvm.webdeveloper2022@gmail.com>
2022-06-03 11:50:04 +0530

    commit

--- eng/settings.py
+++ eng/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 import posixpath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=posixpath.join(*(str(BASE_DIR.resolve(os.path.sep))+'/static'))
+# STATIC_ROOT=posixpath.join(*(str(BASE_DIR.resolve(os.path.sep))+'/static')) 
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'img')
 MEDIA_URL='/img/'
 
@@ -128,7 +129,9 @@ EMAIL_HOST_USER='ajeshmn.767@gmail.com'
 EMAIL_HOST_PASSWORD='7736656141'
 EMAIL_USE_TLS=True
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
