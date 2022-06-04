32f537e0caf3612e8cd5860d7f2db02ca3ec306c
ajesh777mn <kvm.webdeveloper2022@gmail.com>
2022-06-04 10:48:46 +0530

    commit

--- eng/settings.py
+++ eng/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = ['adminapp','btech','mca','mba','basicscience',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
