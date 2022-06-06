7b13e9d2e89dfa24cf2b6e282096e7403d9531ab
ajesh777mn <kvm.webdeveloper2022@gmail.com>
2022-06-06 10:02:35 +0530

    commit

--- eng/settings.py
+++ eng/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1n55hs41!&b!us9%rzd)dt+#u1s$t%noknyro+etrfo$7v%$my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','kvmce.herokuapp.com']
 
